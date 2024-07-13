591b5b6a9d054f6ebb1606439613aa0f276dac1d
h <husnainm3@gmail.com>
2024-07-13 13:50:32 +0100

    Renamed solution and current project. Also added a new project

---
+++ RaindropAutomations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RaindropAutomations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
